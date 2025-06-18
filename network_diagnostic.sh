cec3733f633c3762e18b7a45ced9b3a62b255348
esther mensah <edmworkmail@gmail.com>
2025-06-18 15:57:14 +0200

    syntax errors in script fixed

--- network_diagnostic.sh
+++ network_diagnostic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # target devices 
-TARGETS=("192.168.102" "192.168.103" )
+TARGETS=("192.168.56.102" "192.168.56.103" )
 
 # Destination of log file 
 LOGFILE="network_diagnostic.log"
@@ -13,22 +13,19 @@ echo "----------------------------------------" >> $LOGFILE
 
 
 for IP in "${TARGETS[@]}"; do 
-    echo "Testing $IP..." | tee -a $LOGFILE
+    echo "Testing $IP..." | tee -a "$LOGFILE"
 
-    echo "Ping Test:" | tee -a $LOGFILE
-    ping - c 3 $ip >> $LOGFILE 2>&1
-    echo "-----------------------------" >> $LOGFILE
+    echo "Ping Test:" | tee -a "$LOGFILE"
+    ping -c 3 "$IP" >> "$LOGFILE" 2>&1
+    echo "-----------------------------" >> "$LOGFILE"
 
+    echo "Traceroute :" | tee -a "$LOGFILE"
+    traceroute -m 10 "$IP" >> "$LOGFILE" 2>&1
+    echo "-----------------------------" >> "$LOGFILE"
 
-    echo "Traceroute :" | tee -a $LOGFILE
-    traceroute -m 10 $ip >> $LOGFILE 2&1
-    echo "-----------------------------" >> $LOGFILE
-
-
-    echo "DNS LOOKUP :" | tee -a $LOGFILE
-    nslookup $IP >> $logfile 2&1
-    echo "-----------------------------" >> $LOGFILE
-
+    echo "DNS LOOKUP :" | tee -a "$LOGFILE"
+    nslookup "$IP" >> "$LOGFILE" 2>&1
+    echo "-----------------------------" >> "$LOGFILE"
 done
 
-echo "Diagnotic competed. Results saved to $LOGFILE"
+echo "Diagnostic completed. Results saved to $LOGFILE"
